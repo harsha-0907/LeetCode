800fdf3a9d2cded6a997d412368e08235398d139
goodsirhello807 <goodsirhello807@gmail.com>
2024-11-09 06:58:56 +0000

    Commit Verified

--- potd/medium/findSortedArray.py
+++ potd/medium/findSortedArray.py
@@ -15,3 +15,19 @@ class Solution:
                     else:
                         return False
         return True
+
+    # Optimal
+    def canSortArrayOptimal(self, nums: List[int]) -> bool:
+        pmax = cmin = cmax = pcnt = 0
+        for v in nums:
+            ccnt = v.bit_count()
+            if pcnt == ccnt:
+                cmin = min(cmin, v)
+                cmax = max(cmax, v)
+            elif cmin < pmax:
+                return False
+            else:
+                pmax = cmax
+                cmin = cmax = v
+                pcnt = ccnt
+        return cmin >= pmax
